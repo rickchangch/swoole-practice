a657a706991d8e3e1aa20db2cac6e39eb8f5efdc
Rick Chang <rick.zhang@atelli.ai>
2022-02-21 22:36:39 +0800

    feat: allow root login from any host

--- docker-files/db/init.sql
+++ docker-files/db/init.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE IF NOT EXISTS `test_db`;
-GRANT ALL ON *.* TO 'user'@'%';
-GRANT ALL ON *.* TO 'root'@'%';
+UPDATE mysql.user SET host = '%' WHERE user = 'root';
+GRANT ALL ON main_db.* TO 'user'@'%';
+GRANT ALL ON test_db.* TO 'user'@'%';
 FLUSH PRIVILEGES;
